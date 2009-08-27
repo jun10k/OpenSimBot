9d083dc15ed2b1dbc106427463d52ff4e308597e
koreywan <wanjun81@gmail.com>
2009-08-27 17:50:07 +0800

    Working in progress

---
+++ OMVWrapper/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenSimBot.OMVWrapper.Manager
+{
+    interface IManager
+    {
+        bool Initialize();
+        void Reset();
+        void SetLogLevel();
+    }
+}
